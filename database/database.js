3e5cfa0f37a1ef7a0ec745ea95464c5207795dfd
CosmoAndrade <cosmo.andrade@hotmail.com>
2021-11-08 20:17:23 -0300

    Salvando artigos

--- database/database.js
+++ database/database.js
@@ -4,7 +4,8 @@ const Sequelize = require('sequelize')
 // Criação e coniguração do banco de Dados
 const connection = new Sequelize('guiapress','root','2384',{
     host: 'localhost',
-    dialect:'mysql'
+    dialect:'mysql',
+    timezone: '-03:00'
 })
 
 module.exports = connection;
